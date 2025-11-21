42d82f18fe4fc552356bd0734bffa258b13577eb
arilakshme-05 <arilakshme05@gmail.com>
2025-11-21 21:25:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/contact",methods=["GET","POST"])
+def contact():
+    if request.method=="POST":
+        name=request.form.get("name")
+        email=request.form.get("email")
+        message=request.form.get("message")
+        return render_template("success.html")
+    return render_template("contact.html")
+if __name__=="__main__":
+    app.run(debug=True)
+    
